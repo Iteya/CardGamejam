580105b2533c117e4e2ceb2112a6e54701524866
Iteya <jandrico7658@student.pps.net>
2024-02-29 11:33:06 -0800

    Created Card-Base

--- Assets/Javyn/Scripts/Cards/Card.cs
+++ Assets/Javyn/Scripts/Cards/Card.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "Card", menuName = "Cards/Card", order = 1)]
 public class Card : CardBase
 {
     // Start is called before the first frame update
--- Assets/Javyn/Scripts/Cards/CardBase.cs
+++ Assets/Javyn/Scripts/Cards/CardBase.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CardBase : ScriptableObject
 {
     public GameObject prefab;
-    [TextArea(15, 20)] public string name, description, sManaCost, sDamage;
+    [TextArea(15, 20)] 
+    public string description;
+    [TextArea(1, 10)] public string cardName, sManaCost, sDamage;
     public bool isPoison, isFire;
     public int manaCost, damage;
     public GameObject upgrade;
