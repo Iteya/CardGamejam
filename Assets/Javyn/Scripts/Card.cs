df2a9fea4442466a45b842d2a1be1c1c547bb240
Iteya <jandrico7658@student.pps.net>
2024-03-04 11:41:18 -0800

    Card Hovering Started

--- Assets/Javyn/Scripts/Card.cs
+++ Assets/Javyn/Scripts/Card.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 
 public class Card : MonoBehaviour
 {
     public CardBase data;
     public TextMeshProUGUI tCardName, tManaCost, tDamage, tDescription;
-    public Image image;
+    public Image image, glow;
 
     void Update()
     {
+        #region CardData
         tCardName.text = data.cardName;
         tManaCost.text = data.manaCost.ToString();
         tDamage.text = data.damage.ToString();
@@ -28,5 +30,20 @@ public class Card : MonoBehaviour
         {
             image.color = new Color(0.737f,0.682f,0.682f);
         }
+        #endregion
+        
+        //TODO make card selectable;
+        if (Input.mousePosition.x > transform.position.x - 50 && Input.mousePosition.x < transform.position.x + 50)
+        {
+            glow.color = new Color(1, 1, 0, 1);
+        }
+        else
+        {
+            glow.color = new Color(0, 0, 0, 0);
+        }
+
+
+        Debug.Log(transform.position.x);
+        Debug.Log(Input.mousePosition.x);
     }
 }
