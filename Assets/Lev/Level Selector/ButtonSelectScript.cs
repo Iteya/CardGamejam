c8eb5ec19e5b028213ca6c735821476a623e42b0
Inocybe <levmitchell@icloud.com>
2024-03-08 11:35:57 -0800

    Wans't too productive...

--- Assets/Lev/Level Selector/ButtonSelectScript.cs
+++ Assets/Lev/Level Selector/ButtonSelectScript.cs
@@ -1,8 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelectorScript : MonoBehaviour
 {
+    public static float SelectedLevelWeight;
+    public float levelWeight;
     
+    public void OpenScene()
+    {
+        SelectedLevelWeight = levelWeight;
+        SceneManager.LoadScene("Level Scene");
+    }
 }
